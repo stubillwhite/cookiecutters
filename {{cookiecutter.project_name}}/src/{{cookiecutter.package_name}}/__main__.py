40e8d583b44fd7b7a90453ee7bf3f08cb3227bd2
Stu W <stubillwhite@gmail.com>
2024-04-15 17:29:46 +0100

    Initial version

---
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -0,0 +1,4 @@
+from {{cookiecutter.package_name}}.app import main
+
+if __name__ == "__main__":
+    main()
---
+++ {{cookiecutter.project_name}}/tests/{{cookiecutter.package_name}}/test_app.py
@@ -0,0 +1,5 @@
+from {{cookiecutter.package_name}}.app import main
+
+
+def test_add_five():
+    assert 3 + 5 == 8
